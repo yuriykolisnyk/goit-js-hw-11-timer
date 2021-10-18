e6566f717516d93dc2f60306c408e2b5208d2f2f
yuriykolisnyk <ykolisnyk@gmail.com>
2021-10-18 16:32:31 +0300

    Add const

---
+++ src/js/timer.js
@@ -0,0 +1,4 @@
+const daysEl = document.querySelector('[data-value="days"]');
+const hoursEl = document.querySelector('[data-value="hours"]');
+const minsEl = document.querySelector('[data-value="mins"]');
+const secsEl = document.querySelector('[data-value="secs"]');
